7770db5cd74c9a2a0ecdad1beb780666f3ee01fb
Emily Wise <wise.m.emily@gmail.com>
2017-10-15 18:32:57 -0600

    User Test Passed

--- spec/features/jobs/user_updates_job_spec.rb
+++ spec/features/jobs/user_updates_job_spec.rb
@@ -4,11 +4,11 @@ describe "User can edit an existing job" do
   scenario "a user can create a new job" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    visit new_company_job_path(company)
+    visit edit_company_job_path(company, job)
 
     fill_in "job[title]", with: "Marketing Analyst"
 
-    click_button "Create"
+    click_button "Update Job"
 
     expect(current_path).to eq("/companies/#{Company.last.id}/jobs/#{Job.last.id}")
     expect(page).to have_content("Marketing Analyst")
