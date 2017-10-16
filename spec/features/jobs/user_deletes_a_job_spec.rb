160cf322a5349b4036b376f9cdc2e29644c0ec65
Emily Wise <wise.m.emily@gmail.com>
2017-10-16 15:11:05 -0600

    passing delete test

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -4,9 +4,9 @@ describe "User deletes existing job" do
   scenario "a user can delete a job" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    visit company_job_path
+    visit company_jobs_path(company)
 
-    within("/companies/#{Company.last.id}/jobs") do
+    within(".jobs") do
       click_link "Delete"
     end
 
