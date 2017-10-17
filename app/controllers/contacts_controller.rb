f94ec1a0783d1ab051bb639865cd5e3e161460d1
Emily Wise <wise.m.emily@gmail.com>
2017-10-17 17:32:14 -0600

    Wip Contacts

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,18 @@
+class ContactsController < ApplicationController
+
+  def create
+    @company = Company.find(params[:company_id])
+    @job= Job.find(params[:job_id])
+    @contact = Contact.new(contact_params)
+    @comment.company_id = params[:company_id]
+    @contact.save
+
+    redirect_to company_job_path(@company, @job)
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:full_name, :position, :email)
+  end
+end
