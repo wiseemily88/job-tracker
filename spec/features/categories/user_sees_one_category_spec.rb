e8a0e9ff2f8c6fe1c52572793fd80873253dea07
Emily Wise <wise.m.emily@gmail.com>
2017-10-18 17:21:46 -0600

    added jobs to category page test

--- spec/features/categories/user_sees_one_category_spec.rb
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -9,6 +9,20 @@ describe "User sees one category" do
 
     expect(current_path).to eq(category_path(category))
     expect(page).to have_content("Web Development")
-  
+
+  end
+
+  scenario "a user sees a category they see all jobs associated" do
+    category = Category.create!(title: "Web Development")
+    company = Company.create!(name: "ESPN")
+    company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category: category)
+    company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City", category: category)
+
+
+    visit category_path(category)
+
+    expect(page).to have_content("Developer")
+    expect(page).to have_content("QA Analyst")
+
   end
 end
