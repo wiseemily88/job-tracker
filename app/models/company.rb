f34c52798139dd138e2e697ff120d374617533e6
Emily Wise <wise.m.emily@gmail.com>
2017-10-18 12:15:19 -0600

    Add Top 3 Companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,13 @@ class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :jobs, dependent: :destroy
   has_many :contacts
+
+
+  def self.top_companies_by_level_of_interest
+    select('companies.name, avg(jobs.level_of_interest) AS average')
+    .joins(:jobs)
+    .group('companies.name')
+    .order('avg(jobs.level_of_interest) desc')
+    .first(3)
+  end
 end
