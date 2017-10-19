9352037b338418ac3388663dd00807c732d799bb
Emily Wise <wise.m.emily@gmail.com>
2017-10-18 18:24:30 -0600

    Added Company model tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -29,4 +29,20 @@ describe Company do
       expect(company).to respond_to(:jobs)
     end
   end
+
+  describe "Class methods" do
+
+    it "top_companies_by_level_of_interest" do
+      company_1 = Company.new(name: "Turing")
+      company_2 = Company.new(name: "Disney")
+      company_3 = Company.new(name: "Amazon")
+      category = Category.new(title: "Web Development")
+      job_1 = Job.create!(title: "Developer", level_of_interest: 100, city: "Denver", company: company_1, category: category)
+      job_2 = Job.create!(title: "Developer", level_of_interest: 10, city: "Denver", company: company_2, category: category)
+      job_3 = Job.create!(title: "Developer", level_of_interest: 90, city: "Denver", company: company_3, category: category)
+
+      expect(Company.top_companies_by_level_of_interest.first.name).to eq("Turing")
+    end
+  end
+
 end
